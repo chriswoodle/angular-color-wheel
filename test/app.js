761adfe53c03ed6ee5cd966424356498e405428c
Chris Woodle <chris@droplit.io>
2017-08-08 17:28:05 -0400

    Added binding and range

--- test/app.js
+++ test/app.js
@@ -1,5 +1,11 @@
 'use strict';
 
-angular.module('app', [
+var app = angular.module('app', [
     'droplit.angular-color-picker'
 ]);
+
+app.controller('MyController', ['$scope', function ($scope) {
+    // $scope.hue = 90;
+    $scope.hue = 0x7fff;
+    $scope.range = 0xffff;
+}]);
